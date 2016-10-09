bbb9f545977d1ab2c4605ddf79f97340ce13a8b4
Jasmina Kruhak <jkruhak@gmail.com>
2016-10-09 13:52:13 +0200

    Module 4 assignment skeleton

---
+++ module4-solution/src/menudata.service.js
@@ -0,0 +1,20 @@
+(function() {
+'use strict';
+
+	angular.module('data')
+	.service('MenuDataService', MenuDataService);
+
+	MenuDataService.$inject = []
+	function MenuDataService() {
+		var service = this;
+
+		service.getAllCategories = function() {
+
+		};
+
+		service.getItemsForCategory = function(categoryShortName) {
+
+		};
+	}
+
+}());
